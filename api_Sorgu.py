b866f01257a284ab21f4f15aaa19c3e94188155b
Muhammed-Taha-Bolukbasi <tahabolukbasi0@gmail.com>
2023-12-10 15:48:35 +0000

    api_Sorgu

---
+++ api_Sorgu.py
@@ -0,0 +1,19 @@
+import webbrowser
+import requests
+import json
+
+url = "https://api.thecatapi.com/v1/images/search"
+
+payload = {}
+headers = {}
+
+response = requests.request("GET", url, headers=headers, data=payload)
+
+print(response.text)
+
+if isinstance(response.text, str):
+
+    data = json.loads(response.text)
+
+
+webbrowser.open(data[0]['url'])
